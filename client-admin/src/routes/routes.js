3c90547b90474cc6a3fa611e9ecbcd001ae65d92
HuyGia1443 <132995587+HuyGia1443@users.noreply.github.com>
2024-11-26 08:28:17 +0700

    Update routes.js

--- client-admin/src/routes/routes.js
+++ client-admin/src/routes/routes.js
@@ -1,26 +1,27 @@
-import Home from "../Component/Home/Home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from '../Component/Home/Home';
 import Login from "../Component/Auth/Login";
-import Verify from "../Component/Auth/Verify";
 import Register from "../Component/Auth/Register";
 import RequestPasswordReset from "../Component/Auth/RequestPasswordReset";
 import ResetPassword from "../Component/Auth/ResetPassword";
-
-import ProductList from "../Component/Products/ProductList";
-import CategoryList from "../Component/Categories/CategoryList";
-import UserList from "../Component/User/UserList";
-import OrderList from "../Component/Orders/OrderList";
+import ProductList from '../Component/Products/ProductList';
+import CategoryList from '../Component/Categories/CategoryList';
+import UserList from '../Component/User/UserList';
+import OrderList from '../Component/Orders/OrderList';
+import Verify from "../Component/Auth/Verify";
 
 const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/login", element: <Login /> },
-  { path: "/verify", element: <Verify /> },
-  { path: "/register", element: <Register /> },
-  { path: "/auth/request-password-reset", element: <RequestPasswordReset /> },
-  { path: "/reset-password/:token", element: <ResetPassword /> },
-  { path: "/products", element: <ProductList /> },
-  { path: "/categories", element: <CategoryList /> },
-  { path: "/users", element: <UserList /> },
-  { path: "/orders", element: <OrderList /> },
+  { path: '/', element: <Navigate to="/login" /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+  { path: '/verify', element: <Verify />},
+  { path: '/resetpassword', element: <ResetPassword /> },
+  { path: '/home', element: <Home />},
+  { path: '/products', element: <ProductList /> },
+  { path: '/categories', element: <CategoryList /> },
+  { path: '/users', element: <UserList /> },
+  { path: '/orders', element: <OrderList /> },
 ];
 
 export default routes;
