23691e1d4e1bd90c15014727138f5495c955bc7e
HuyGia1443 <132995587+HuyGia1443@users.noreply.github.com>
2024-11-26 08:22:27 +0700

    Update ResetPassword.js

--- client-admin/src/Component/Auth/ResetPassword.js
+++ client-admin/src/Component/Auth/ResetPassword.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import API from "../../api/api";
+import "./ResetPassword.css";
 
 const ResetPassword = () => {
   const { token } = useParams();
@@ -19,7 +20,7 @@ const ResetPassword = () => {
     try {
       await API.post(
         "/auth/reset-password",
-        { newPassword, confirmNewPassword },
+        { newPassword },
         {
           headers: {
             Authorization: token,
@@ -34,31 +35,55 @@ const ResetPassword = () => {
       setSuccess("");
     }
   };
+
   return (
-    <div>
-      <h2>Reset Your Password</h2>
+    <div className="reset-password-container">
+      <h2>Reset Password</h2>
+      <div className="password-requirements">
+        <ul>
+          <li>No repetition of more than two characters</li>
+          <li>One number</li>
+          <li>
+            At least 1 special character from `!@#$%^&*-_/+=`
+          </li>
+          <li>One lowercase character</li>
+          <li>One uppercase character</li>
+          <li>8 characters minimum</li>
+        </ul>
+      </div>
       {error && <p style={{ color: "red" }}>{error}</p>}
       {success && <p style={{ color: "green" }}>{success}</p>}
       <form onSubmit={handleSubmit}>
         <div>
-          <label>New Password</label>
           <input
             type="password"
+            placeholder="New Password*"
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
             required
           />
         </div>
         <div>
-          <label>Confirm New Password</label>
           <input
             type="password"
+            placeholder="Confirm New Password*"
             value={confirmNewPassword}
             onChange={(e) => setConfirmNewPassword(e.target.value)}
             required
           />
         </div>
-        <button type="submit">Reset Password</button>
+        <div className="form-actions">
+          <button
+            type="button"
+            className="cancel-button"
+            onClick={() => navigate("/login")}
+          >
+            Cancel
+          </button>
+          <button type="submit" className="save-button">
+            Save
+          </button>
+        </div>
       </form>
     </div>
   );
